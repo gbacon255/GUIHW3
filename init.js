0cbdcb356febb05b6b8a41b4e0513de59875a5a9
gbacon255 <bacongarrett13@gmail.com>
2024-06-03 17:39:08 -0400

    formatting changes

--- init.js
+++ init.js
@@ -1,3 +1,5 @@
+
+
 //Function to collect and validate our data when the submit button is pressed
 function initializeData(){
 
@@ -7,34 +9,60 @@ function initializeData(){
     const sideStart = document.getElementById('sideStart').value;
     const sideEnd = document.getElementById('sideEnd').value;
 
+    if(topStart == ""|| topEnd.value == ""|| sideStart.value == ""|| sideEnd.value==""){
+        resultDiv.innerHTML = '';
+        error.innerHTML = `<h2>Error:</h2><p>Please Fill the Form with Valid Numbers (-75 to 75)</p>`;
+        return;
+    }
+
     //convert string values to numbers
     const topStartNum = Number(topStart);
     const topEndNum = Number(topEnd);
     const sideStartNum = Number(sideStart);
     const sideEndNum = Number(sideEnd);
+    //get the div elements to access later one for error case and one for the table
+    const errorDiv = document.getElementById('error')
     const resultDiv = document.getElementById('result');
+
+    //error handling for if a non number is entered, html form also prevents this
     if (isNaN(topStartNum) || isNaN(topEndNum)|| isNaN(sideStartNum) ){
-        resultDiv.textContent = `Please Enter Valid Numbers (-75 to 75)`;
+        //this code will throw a message to the error div and return 
+        resultDiv.innerHTML = '';
+        error.innerHTML = `<h2>Error:</h2><p>Please Enter Valid Numbers (-75 to 75)</p>`;
         return;
     }
+    //Check to see if the numbers are in our specified range
     if (!(topStartNum >= -75 && topStartNum <= 75 && sideStartNum >= -75 && sideEndNum <=75)) {
-        resultDiv.textContent = `Please Enter Valid Numbers (-75 to 75)`;
+        resultDiv.innerHTML = '';
+        error.innerHTML = `<h2>Error:</h2><p>Please Enter Valid Numbers (-75 to 75)</p>`;
         return;
     }
+    //if the end number is larger than the start number for either axis we throw an error
     if(topStartNum>topEndNum || sideStartNum > sideEndNum){
-        resultDiv.textContent = 'Please ensure the end number is greater than the start number'
+        resultDiv.innerHTML = '';
+        error.innerHTML = '<h2>Error:</h2><p>Please ensure the end number is greater than the start number</p>'
+        return;
     }
+    //Success case, lets generate the table
      else{
-        resultDiv.textContent = 'valid numbers, generating table';
-        const table = generateTable(topStartNum,topEndNum,sideStartNum,sideEndNum);
-        resultDiv.innerHTML = ''; //clear html for resultdiv
-        resultDiv.innerHTML = table; //display our newly generated table
+        error.innerHTML = '';
+        //loading message
+        resultDiv.innerHTML = '<p>Valid numbers entered, generating table...<p>';
+        //Added a delay for fun to do a fake loading time
+        setTimeout(function(){
+            const table = generateTable(topStartNum,topEndNum,sideStartNum,sideEndNum); //function returns table object
+            resultDiv.innerHTML = ''; //clear html for resultdiv
+            error.innerHTML = ''; // clear error inner HTML
+            resultDiv.innerHTML = table; //display our newly generated table
+        }, 1500);
     }
     return;
 }
 
 function generateTable(xStart, xEnd, yStart,yEnd){
+    //initialize table html object
     let table = '<table><tr><th>-</th>';
+    //create the top row of the table ${} allows us to include the var in our text
     for (i = xStart; i <= xEnd; i++){
         table += `<th> ${i} </th>`;
     }
@@ -50,6 +78,6 @@ function generateTable(xStart, xEnd, yStart,yEnd){
         //terminate the row
         table +='</tr>'
     }
-    //return our html object table
+    //return our html table object
     return table;;
 }
